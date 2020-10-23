96683bda839eca64ddd1a409a784c451703e0632
Richard W. L. Brehmer <richard@bnu.com.br>
2020-10-23 15:05:34 -0300

    refact

--- rest-with-springboot/src/main/java/br/com/erudio/MathController.java
+++ rest-with-springboot/src/main/java/br/com/erudio/MathController.java
@@ -6,33 +6,49 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.erudio.exception.UnsupportedMathOperationException;
+import br.com.erudio.math.SimpleMath;
+import br.com.erudio.request.converters.NumberConverter;
 
 @RestController
 public class MathController {
-
+	
+	private SimpleMath math = new SimpleMath();
 
     @RequestMapping(value="/sum/{numberOne}/{numberTwo}", method=RequestMethod.GET)
     public Double sum( 	@PathVariable(value="numberOne") String numberOne ,
     					@PathVariable(value="numberTwo") String numberTwo) throws Exception {
-    	if(!isNumeric(numberOne) || !isNumeric(numberTwo) ) {
+    	if(!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo) ) {
     		throw new UnsupportedMathOperationException("Please set a numeric value") ;
     	}
-    	Double sum = convertToDouble(numberOne) + convertToDouble(numberTwo);
-    	return sum;
+    	return math.sum(NumberConverter.convertToDouble(numberOne), NumberConverter.convertToDouble(numberTwo));
     }
-
-    private Double convertToDouble(String strNumber) {
-    	if(strNumber == null ) return 0D;
-    	String number = strNumber.replaceAll(",", ".");
-    	if(isNumeric(number)) return Double.parseDouble(number);  
-		return 0D;
-	}
-
-	private boolean isNumeric(String strNumber) {
-		if(strNumber == null ) return false;
-		String number = strNumber.replaceAll(",", ".");
-		return number.matches("[-+]?[0-9]*\\.?[0-9]+");
-	}
+    
+    @RequestMapping(value="/sub/{numberOne}/{numberTwo}", method=RequestMethod.GET)
+    public Double sub( 	@PathVariable(value="numberOne") String numberOne ,
+    					@PathVariable(value="numberTwo") String numberTwo) throws Exception {
+    	if(!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo) ) {
+    		throw new UnsupportedMathOperationException("Please set a numeric value") ;
+    	}
+    	return math.sub(NumberConverter.convertToDouble(numberOne), NumberConverter.convertToDouble(numberTwo));
+    }
+    
+    @RequestMapping(value="/div/{numberOne}/{numberTwo}", method=RequestMethod.GET)
+    public Double div( 	@PathVariable(value="numberOne") String numberOne ,
+    					@PathVariable(value="numberTwo") String numberTwo) throws Exception {
+    	if(!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo) ) {
+    		throw new UnsupportedMathOperationException("Please set a numeric value") ;
+    	}
+    	return math.div(NumberConverter.convertToDouble(numberOne), NumberConverter.convertToDouble(numberTwo));
+    }
+    
+    @RequestMapping(value="/med/{numberOne}/{numberTwo}", method=RequestMethod.GET)
+    public Double med( 	@PathVariable(value="numberOne") String numberOne ,
+    					@PathVariable(value="numberTwo") String numberTwo) throws Exception {
+    	if(!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo) ) {
+    		throw new UnsupportedMathOperationException("Please set a numeric value") ;
+    	}
+    	return math.med(NumberConverter.convertToDouble(numberOne), NumberConverter.convertToDouble(numberTwo));
+    }
 
 	@RequestMapping("/hello")
     public String hello() {
---
+++ rest-with-springboot/src/main/java/br/com/erudio/math/SimpleMath.java
@@ -0,0 +1,25 @@
+package br.com.erudio.math;
+
+public class SimpleMath {
+	
+	public Double sum(Double firstNumber, Double secondNumber) {
+		return firstNumber + secondNumber;
+	}
+	
+	public Double sub(Double firstNumber, Double secondNumber) {
+		return firstNumber - secondNumber;
+	}
+	
+	public Double div(Double firstNumber, Double secondNumber) {
+		return firstNumber / secondNumber;
+	}
+	
+	public Double mult(Double firstNumber, Double secondNumber) {
+		return firstNumber * secondNumber;
+	}
+	
+	public Double med(Double firstNumber, Double secondNumber) {
+		return (firstNumber + secondNumber) / 2;
+	}
+
+}
